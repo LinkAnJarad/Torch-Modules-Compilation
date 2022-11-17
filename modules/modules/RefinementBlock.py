f8a1950f94f78cacd2a891cf67086e0fc0321cdf
LinkAnJarad <79294502+LinkAnJarad@users.noreply.github.com>
2022-11-17 19:40:01 +0800

    Create RefinementBlock.py

---
+++ modules/modules/RefinementBlock.py
@@ -0,0 +1,17 @@
+import torch
+from torch import nn
+from .TransformerEncoderLayer import TransformerEncoderLayer
+
+class RefinementBlock(nn.Module):
+  '''
+  Learning Token-Based Representation for Image Retrieval
+  '''
+  def __init__(self, d_model, nhead, dim_feedforward, dropout):
+    super().__init__()
+    self.self_att = TransformerEncoderLayer(d_model, nhead, dim_feedforward, dropout=dropout) # Batch first
+    self.cross_att = TransformerEncoderLayer(d_model, nhead, dim_feedforward, dropout=dropout) # Batch first
+
+  def forward(self, visual_tokens, cnn_output):
+    self_att = self.self_att(visual_tokens, visual_tokens, visual_tokens)
+    cross_att = self.cross_att(self_att, cnn_output, cnn_output)
+    return 
