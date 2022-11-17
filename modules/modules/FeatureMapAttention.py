24788713a4b9c919b002570b7d6512a6a2197d67
LinkAnJarad <79294502+LinkAnJarad@users.noreply.github.com>
2022-11-17 20:27:14 +0800

    Update FeatureMapAttention.py

--- modules/modules/FeatureMapAttention.py
+++ modules/modules/FeatureMapAttention.py
@@ -1,12 +1,19 @@
 import torch
 from torch import nn
 
-class FeatureMapAttention(nn.Module):
+class FeatureMapSelfAttention(nn.Module):
     '''
-    Adapted from https://github.com/rosinality/sagan-pytorch/blob/master/model.py#L82
+    Copied from https://github.com/rosinality/sagan-pytorch/blob/master/model.py#L82 under Apache 2.0 License.
+    A feature map self-attention module used in SAGAN; "Self-Attention Generative Adversarial Networks" (https://arxiv.org/pdf/1805.08318.pdf)
+    
+    Parameters:
+    in_channels (int): Number of input channels
+    
+    Returns:
+    Tensor of shape (batch_size, channels, height, width); same shape as input
     '''
     def __init__(self, in_channels):
-        super(FeatureMapAttention, self).__init__()
+        super(FeatureMapSelfAttention, self).__init__()
         
         self.qw = nn.Conv1d(in_channels, in_channels, 1, 1)
         self.kw = nn.Conv1d(in_channels, in_channels, 1, 1)
@@ -14,19 +21,15 @@ class FeatureMapAttention(nn.Module):
         
         self.gamma = nn.Parameter(torch.tensor(0.0))
         
-    def forward(self, query, key, value):
-        shape = query.shape
-        flatten_q = query.view(shape[0], shape[1], -1)
-        flatten_k = key.view(shape[0], shape[1], -1)
-        flatten_v = value.view(shape[0], shape[1], -1)
-
-        query = self.qw(flatten_q).permute(0, 2, 1)
-        key = self.kw(flatten_k)
-        value = self.vw(flatten_v)
+    def forward(self, x):
+        shape = x.shape
+        flatten = x.view(shape[0], shape[1], -1)
+        query = self.qw(flatten).permute(0, 2, 1)
+        key = self.kw(flatten)
+        value = self.vw(flatten)
         query_key = torch.bmm(query, key) / shape[-1]
         attn = query_key.softmax(dim=1)
         attn = torch.bmm(value, attn)
         attn = attn.view(*shape)
-        value = value.view(*shape)
-        out = self.gamma * attn + value
+        out = self.gamma * attn + x
         return out
