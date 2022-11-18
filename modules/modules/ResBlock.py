0b285a3e84ca4948f8e20090a466605b67023d5b
LinkAnJarad <79294502+LinkAnJarad@users.noreply.github.com>
2022-11-18 09:35:09 +0800

    Update ResBlock.py

--- modules/modules/ResBlock.py
+++ modules/modules/ResBlock.py
@@ -5,6 +5,14 @@ from torch import nn
 class ResBlock(nn.Module):
     '''
     Your basic residual block.
+    
+    Parameters:
+    in_channels (int): number of input channels
+    kernel_size (int): kernel size
+    dropout (float): dropout rate
+    
+    Returns:
+    Tensor of shape (batch_size, channels, height, width); same as input
     '''
     def __init__(self, in_channels, kernel_size, dropout):
         super().__init__()
