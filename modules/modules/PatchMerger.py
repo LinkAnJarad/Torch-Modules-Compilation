5b47eaae4ce000cb124c59a1d4d8fbfdf09cd6ec
LinkAnJarad <79294502+LinkAnJarad@users.noreply.github.com>
2022-11-18 09:25:21 +0800

    add docstring

--- modules/modules/PatchMerger.py
+++ modules/modules/PatchMerger.py
@@ -4,8 +4,19 @@ from torch import nn
       
 class PatchMerger(nn.Module):
     '''
-    Copied from https://github.com/lucidrains/vit-pytorch/blob/main/vit_pytorch/vit_with_patch_merger.py
-    Paper from https://arxiv.org/pdf/2202.12015v1.pdf
+    Copied from lucidrains' repo https://github.com/lucidrains/vit-pytorch/blob/main/vit_pytorch/vit_with_patch_merger.py under the MIT license.
+    From "LEARNING TO MERGE TOKENS IN VISION TRANSFORMERS" (https://arxiv.org/pdf/2202.12015v1.pdf)
+    
+    Merges N tokens into M tokens in transformer models. Typically added in-between transformer layers.
+    
+    Parameters:
+    dim (int): dimensionality/channels of the tokens
+    output_tokens (int): number of output merged tokens
+    norm (bool): normalize the input before merging
+    scale (bool): scale the attention matrix by the square root of dim (for numerical stability)
+    
+    Returns:
+    Tensor of shape (batch_size, output_tokens, dim)
     '''
     def __init__(self, dim, output_tokens, norm=False, scale=False):
         super(PatchMerger, self).__init__()
