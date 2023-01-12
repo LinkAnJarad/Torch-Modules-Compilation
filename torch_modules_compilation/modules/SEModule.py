5827b3ed5cf8b090129a803bade97bc22a42002a
LinkAnJarad <79294502+LinkAnJarad@users.noreply.github.com>
2023-01-12 12:32:11 +0800

    docstring updated

--- torch_modules_compilation/modules/SEModule.py
+++ torch_modules_compilation/modules/SEModule.py
@@ -3,7 +3,16 @@ from torch import nn
 
 class SEModule(nn.Module):
     '''
-    https://arxiv.org/pdf/1709.01507v4.pdf
+    Module that computes channel-wise interactions in a feature map. From https://arxiv.org/pdf/1709.01507v4.pdf
+    
+    Parameters
+    
+    in_channels (int): Number of input channels
+    reduced_channels (int): Number of channels to reduce to in the "squeeze" part of the module
+    feature_map_size (int): height/width of the feature map
+    
+    Returns:
+    Tensor of shape (batch_size, channels, height, width), same as input
     '''
     def __init__(self, in_channels, reduced_channels, feature_map_size):
         super().__init__()
